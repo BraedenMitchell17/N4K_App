c2e7ca3b4b7aaafdc1ed343d581cd1027fad6e1b
Braeden Mitchell <Braeden.Mitchell@mymhc.ca>
2022-02-04 17:32:11 -0700

    inital application added

---
+++ NFTAppV3/NFTAppV3/NFTAppV3/Views/ShareKissPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NFTAppV3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShareKissPage : ContentPage
+    {
+        public ShareKissPage(string duration, string type)
+        {
+            InitializeComponent();
+            lblDuration.Text = "Duration: " + duration;
+            KissType.Text = type;
+        }
+
+        async void OnNextPageButtonClickedConfirm(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ShareConfirmPage());
+        }
+
+        async void OnNextPageButtonClickedBack(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new KissPage());
+        }
+    }
+}
